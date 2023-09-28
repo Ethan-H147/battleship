7da8696a1eb63653e009c89df2ac9283c5af966c
Ethan Hu <ethanhu147@gmail.com>
2023-09-28 13:04:27 -0400

    now it can make us select 5 coordinates for ships

--- Board.java
+++ Board.java
@@ -5,15 +5,18 @@ public class Board {
     private int column = 10;
     int[] ships = new int[5];
 
-    int[][] game = new int[row][column];
+    int [][] game = new int[row][column];
 
 
 
 public void printBoard(){
     Scanner scan = new Scanner(System.in);
-    int x = scan.nextInt - 1;
-    int y = scan.nextInt - 1;
-    game[x][y] = 3;
+    int co;
+    for(int i = 0; i < 5; i++){
+    co = scan.nextInt() - 1;
+    game[(int)(co/10-1)][co%10] = 3;
+    }
+    
     for (int r = 0; r <= column-1; r++){
         System.out.print("\n");
         for (int c = 0; c <= row-1; c++){
