f71a6e1433c81ded495af965d250744a5b372169
Ethan Hu <ethanhu147@gmail.com>
2023-09-28 12:55:47 -0400

    Not compliling, but I'm trying to let people choos -e where to put ships

--- Board.java
+++ Board.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Board {
-    int row = 10;
-    int column = 10;
+    private int row = 10;
+    private int column = 10;
     int[] ships = new int[5];
 
     int[][] game = new int[row][column];
@@ -8,11 +10,15 @@ public class Board {
 
 
 public void printBoard(){
-    for (int r = 0; r <= column; r++){
-        for (int c = 0; c <= row; c++){
+    Scanner scan = new Scanner(System.in);
+    int x = scan.nextInt - 1;
+    int y = scan.nextInt - 1;
+    game[x][y] = 3;
+    for (int r = 0; r <= column-1; r++){
+        System.out.print("\n");
+        for (int c = 0; c <= row-1; c++){
     System.out.print(game[r][c]);
     }
 }
-System.out.print("\n");
 }
 }
