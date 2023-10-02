8ba4a4458c8ecd2ba971af075472bea322e90074
Ethan Hu <ethanhu147@gmail.com>
2023-10-02 12:44:48 -0400

    Paving the way to create two gametypes

--- Tester.java
+++ Tester.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Tester{
      public static void main(String[] args){
-        Board ten = new Board();
+      System.out.println("Fast/Regular Game?(F/R)");
+      Board ten = new Board();
+      Scanner scan = new Scanner(System.in);
+      String gametype = scan.nextLine();
+      if(gametype.equals("R")||gametype.equals("r"))
         ten.placeShip();
      }
 }
