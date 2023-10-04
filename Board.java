d93b3e1cc796db2382c6fd3e4c37f61c598b41c5
Ethan Hu <ethanhu147@gmail.com>
2023-10-04 12:23:52 -0400

    simplified the code by switching a bunch of if statement to a case switching

--- Board.java
+++ Board.java
@@ -30,44 +30,38 @@ public void placeShip(String shipType, int shipSize){
 
     System.out.println("East(1),North(2),West(3),South(4)");
     int facing = scan.nextInt();
-    if(facing == 1){
         for(int i=1; i<shipSize; i++){
+            switch(facing){
+            case 1: 
             game[x][y+i] = shipSize;
+            case 2:
+            game[x-i][y] = shipSize;
+            case 3:
+            game[x][y-i] = shipSize;
+            case 4:
+            game[x+i][y] = shipSize;
+            }
         }
+        printBoard();
     }
-        else if(facing == 2){
-            for(int i=1; i<shipSize; i++){
-                game[x-i][y] = shipSize;
-            }
-        }
-        else if(facing == 3){
-            for(int i=1; i<shipSize; i++){
-                game[x][y-i] = shipSize;
-            }
-        }
-        else if(facing == 4){
-            for(int i=1; i<shipSize; i++){
-                game[x+i][y] = shipSize;
-            }
-        }
-printBoard();
-    }
+    
+    
    
    
-private boolean isOccupied(int x, int y){
-    return game[x][y]!=0;
-} 
+    private boolean isOccupied(int x, int y){
+        return game[x][y]!=0;
+    } 
 
-private boolean isOutofbound(int x, int y){
-    return x<0||x>row||y<0||y>column;
-} 
+    private boolean isOutofbound(int x, int y){
+        return x<0||x>row||y<0||y>column;
+    } 
    
- public void printBoard(){
-    for (int r = 0; r <= column-1; r++){
-        System.out.print("\n");
-        for (int c = 0; c <= row-1; c++){
-    System.out.print(game[r][c]+" ");
+    public void printBoard(){
+        for (int r = 0; r <= column-1; r++){
+            System.out.print("\n");
+            for (int c = 0; c <= row-1; c++){
+                System.out.print(game[r][c]+" ");
+            }
+        }
     }
-}
-}
 }
