92cae093336b14a8484ce442fce2b177670034e2
Ethan-H147 <97569719+Ethan-H147@users.noreply.github.com>
2023-10-11 17:36:19 -0400

    created a new class SmallBoard for fast game

---
+++ SmallBoard.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SmallBoard {
+    Scanner scan = new Scanner(System.in);
+    private int row = 8;//set the size of the board
+    private int column = 8;
+
+    int [][] game = new int[row][column];
+    char [][] guess = new char[row][column];
+    
+    int x;  
+    int y;
+
+}
