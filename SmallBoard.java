4d5faae445086521dca899edce53df6272bdeb82
Ethan-H147 <97569719+Ethan-H147@users.noreply.github.com>
2023-10-11 17:40:20 -0400

    replicated printBoard from regular game, although in this class it will only print 8x8

--- SmallBoard.java
+++ SmallBoard.java
@@ -10,4 +10,12 @@ public class SmallBoard {
     int x;  
     int y;
 
+    public void printBoard(){//print the 8x8 board with ships,only for testing
+            for (int r = 0; r <= column-1; r++){
+                System.out.print("\n");
+                for (int c = 0; c <= row-1; c++){
+                    System.out.print(game[r][c]+" ");
+                }
+            }
+    }
 }
