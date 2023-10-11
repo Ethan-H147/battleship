6df6850c547598c3838471d607b8aebee55bb71e
Ethan Hu <ethanhu147@gmail.com>
2023-10-11 12:09:02 -0400

    added different case, players can put in different letters to get different instructions

--- Instruction.java
+++ Instruction.java
@@ -6,6 +6,32 @@ public class Instruction {
         System.out.println("Instructions:Basic Rules-B||Placement Rules-P||Gametypes-G||Skip-S");
         String i = scan.nextLine();
         switch(i){
+            case "B":
+                System.out.print("- First player has a fleet of ships and a board to place those ships.\n" + 
+                        "- Second player will guess the coordinates of the opponent's ships.\n" + 
+                        "- Coordinates are represented by two digit integers, the first digit is the row number and the second is column.\n" +
+                        "- In a 10x10 board, numbers are labelled from 0 to 9, so the top left is 00 and the bottom right is 99.\n" +
+                        "- A hit is recorded with an 'X' when a guess lands on a part of a ship.\n" + 
+                        "- A miss is recorded with a 'O' when a guess lands on an empty cell.\n" + 
+                        "- The game continues until the first player's fleet is completely destroyed.\n");
+                for(int j=0;j<10;j++){
+                    System.out.print(j+" ");
+                }
+                for (int r = 0; r < 10; r++){
+                    System.out.print("\n");
+                    for (int c = 0; c < 10; c++){
+                        System.out.print("0 ");
+                    }
+                    System.out.print(r);
+                }
+                System.out.print("\n");
+                break;
+            case "P":
+            break;
+            case "G":
+            break;
+            case "S":
+            break;
         }
     }
 }
